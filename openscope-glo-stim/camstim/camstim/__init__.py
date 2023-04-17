4644c3dcefd21fb338654c4102e7fcc28705a252
Akansha Lalwani <alalwani@ucsd.edu>
2023-04-16 17:06:27 -0700

    Delete openscope-glo-stim directory

--- openscope-glo-stim/camstim/camstim/__init__.py
+++
@@ -1,7 +0,0 @@
-from sweepstim import SweepStim, Stimulus, NaturalScenes, MovieStim
-from behavior import Behavior, Foraging, VisualObject
-from experiment import Experiment, Timetrials
-from window import Window, Warp
-from misc import ImageStimNumpyuByte
-
-__version__ = "0.2.4"
