115117992eeef7282e7ca7594c9e1901bc1bce35
Akansha Lalwani <alalwani@ucsd.edu>
2023-05-01 16:47:09 -0700

    add drifing grating script

--- test-scripts/drifting_grating.py
+++ test-scripts/drifting_grating.py
@@ -1,11 +1,43 @@
 """
 brain_observatory_stimulus.py
 """
-# Import required modules
+
+################# Parameters #################
+FPS = 30
+testmode = True
+SessionDuration = 120
+TimeDilation = 1
+MAXRPT = 32
+SPATIALFREQ = [0.02, 0.04, 0.08]
+ORIENTATIONS = [0, 90]
+PHASES = [0, 90]
+DRIFTRATES = [12, 24]
+NCOND = len(SPATIALFREQ)*len(PHASES)*len(ORIENTATIONS)
+##############################################
+
+# Import necessary libraries
+from psychopy import monitors, visual
 from camstim import Stimulus, SweepStim
 from camstim import Window, Warp
-from psychopy import monitors, visual
 import os
+import time
+import numpy as np
+
+# read Contrast information from comma separated file
+path = os.path.dirname(os.path.abspath(__file__))
+
+with open(path + r"\TestSequenceOnes.txt") as f:
+    Phase = f.readlines()
+    Phase = [x.split(',') for x in Phase]   
+    Phase = [x for x in Phase if len(x) > 1]
+    Phase = [[y.strip() for y in x] for x in Phase]
+    ## remove empty strings
+    Phase = [[y for y in x if y] for x in Phase]
+    ## convert to float
+    Phase = [[float(y) for y in x] for x in Phase]
+
+# Get path of current file
+path = os.path.dirname(os.path.abspath(__file__))
 
 # Define monitor settings 
 dist = 15.0
@@ -19,24 +51,8 @@ window = Window(fullscr=True,
                 # monitor= 'Gamma1.Luminance50',          # MS 
                 monitor = monitor,
                 screen=1,
-                warp=Warp.Spherical,
+                # warp=Warp.Spherical,
                 )
-
-
-
-path = os.path.dirname(os.path.abspath(__file__))
-
-# with open(path + r"\TestSequenceBWN.txt") as f:
-with open(path + r"\TestSequenceRamp.txt") as f:
-    Color = f.readlines()
-    Color = [x.split(',') for x in Color]   
-    Color = [x for x in Color if len(x) > 1]
-    Color = [[y.strip() for y in x] for x in Color]
-    ## remove empty strings
-    Color = [[y for y in x if y] for x in Color]
-    ## convert to float
-    Color = [[float(y) for y in x] for x in Color]
-
 
 # Load the stimulus from file 
 dg = Stimulus(visual.GratingStim(window,
@@ -49,19 +65,20 @@ dg = Stimulus(visual.GratingStim(window,
                     sf=0.1,
                     ),
     sweep_params={
-               'Contrast': ([0.8], 0),
-               'TF': ([4.0], 1),
-               'SF': ([0.04], 2),
-               'Ori': ([45], 3),
-               },
+               'Contrast': ([10], 0),
+               'SF': (SPATIALFREQ, 1),
+               'Ori': (ORIENTATIONS, 2),
+               'Phase': (Phase[0], 3),
+               'TF': ([1], 4),
+    },
     sweep_length=2.0,
     start_time=0.0,
     blank_length=0,
     blank_sweeps=0,
-    runs=15,
+    runs=1,
     shuffle=False,
     save_sweep_table=True,
-    # kframes = 1200,
+    kframes = 1
     )
 
 # Define rhe display sequence for the stimulus 
